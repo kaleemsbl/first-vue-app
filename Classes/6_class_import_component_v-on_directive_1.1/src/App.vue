a954630d19d8f5a4877f727f356e3339ba933b92
Kaleem Khan <kaleemkhan05071995>
2022-09-18 12:48:48 +0530

    rest of bult in directive

---
+++ Classes/6_class_import_component_v-on_directive_1.1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="parent">
+    <h1 class="text-align-center">Parent</h1>
+
+    <div class="component_wrapper">
+      <TestComponent />
+    </div>
+
+    <div class="component_wrapper">
+      <TestCompo />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import TestComponent from './components/TestComponent.vue';
+import TestCompo from './components/TestCompo.vue'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    TestComponent,
+    TestCompo
+  }
+
+}
+</script>
+
+<style>
+.parent {
+  height: 100vh;
+  background: rgba(42, 185, 228, 0.51);
+}
+
+.component_wrapper {
+  width: 80%;
+  margin: 10px auto 0px auto;
+}
+</style>
---
+++ Classes/7_class_directive_2.5/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="parent text-align-center">
+
+    <h1>v-on directive</h1>
+    <br>
+    <div class="component_wrapper">
+      <button v-on:click="increment">Increment</button>
+      <br><br>
+      {{ counter }}
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  components: {
+    // define component here to be used in html
+  },
+  methods: {
+    increment: function () {
+      this.counter++;
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
