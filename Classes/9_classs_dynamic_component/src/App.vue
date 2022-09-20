62466b7a88dc1a30bf1a4f27c8bcebdd40bf433b
Kaleem Khan <kaleemkhan05071995>
2022-09-20 15:57:07 +0530

    Computed property

---
+++ Classes/9_classs_dynamic_component/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="parent-center">
+    <div class="container">
+      <div class="row mb-3">
+        <div class="col">
+          <h2>Dynamic Components</h2>
+        </div>
+      </div>
+
+      <div class="row mb-5">
+        <div class="col-sm-3">
+          <button type="button" class="btn btn-primary" @click="changeComponent('PercentCalculate')">Calculate
+            %</button>
+        </div>
+        <div class="col-sm-3">
+          <button type="button" class="btn btn-success" @click="changeComponent('incrementDecrement')">Counter</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <component :is="selectedComponent"></component>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import PercentCalculate from './components/PercentCalculate.vue';
+import incrementDecrement from './components/IncDec.vue';
+export default {
+  data() {
+    return {
+      selectedComponent: ""
+    }
+  },
+  components: {
+    PercentCalculate,
+    incrementDecrement
+  },
+  methods: {
+    changeComponent(changer) {
+      this.selectedComponent = changer;
+    }
+  }
+}
+</script>
---
+++ Classes/9_classs_dynamic_component/src/components/PercentCalculate.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="heading">
+        <h2>Find %</h2>
+    </div>
+
+    <div class="form-group">
+        <input class="form-control" type="text" v-model="amount" placeholder="Enter Total value" />
+    </div>
+
+
+
+    <div class="form-group">
+        <input class="form-control" type="text" v-model="finder" placeholder="Enter % value " />
+    </div>
+
+
+    <div class="row" v-if="amount && finder != ''">
+        <div class="col display-3">
+            Result : {{ (amount * finder) / 100 }}
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            amount: null,
+            finder: null,
+        };
+    },
+};
+</script>
