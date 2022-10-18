32701a75f1001c0a9b55e944186fe009aa9337dd
Kaleem Khan <kaleemkhan05071995>
2022-10-18 10:06:35 +0530

    Props and Loader implementation

---
+++ Classes/17_Props_Validation_impl_loader/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="parent-center">
+    <div class="container">
+      <div v-if="userListShow">
+        <UsersList @editUser="editUser" />
+      </div>
+      <div v-else>
+        <EditUser :userSingleData="user" @updatSuccess="updatSuccess" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UsersList from "./components/users/UsersList.vue";
+import EditUser from "./components/users/EditUser.vue";
+export default {
+  data() {
+    return {
+      userListShow: true,
+      componentName: 'UsersList',
+      user: null
+    }
+  },
+  components: {
+    UsersList,
+    EditUser
+  },
+  methods: {
+    async editUser(userData) {
+      this.user = userData;
+      this.userListShow = false;
+    },
+    updatSuccess(status) {
+      this.userListShow = true;
+    }
+  }
+}
+</script>
---
+++ src/components/loader/Loader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row loaderHeight" v-if="show">
+    <div class="col text-center loaderContent">
+      <img :src="`/public/loader/${name}.gif`" :width="width">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default() {
+        return 'loader_1';
+      }
+    },
+    show: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+    width: {
+      type: String,
+      default() {
+        return '200';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loaderHeight {
+  height: 100vh;
+}
+
+.loaderContent {
+  margin: auto;
+}
+</style>
